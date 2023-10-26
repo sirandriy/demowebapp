40a1c595bb207f0a0fae7ec0f7ace737badc65be
sirandriy <andriyko.hlushko@gmail.com>
2023-10-26 20:26:13 +0300

    Initial commit

---
+++ src/test/java/com/slonyk/demowebapp/DemowebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slonyk.demowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
